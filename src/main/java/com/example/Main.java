3deacfe60f0760e501a88a4078777442369f9d7d
Mariana Velez <marianagv2222@gmail.com>
2026-02-24 08:24:33 -0500

    avance

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Libro l1 = new Libro(); 
+        l1.mostrarDetalles();
+
+        Libro l2 = new Libro("Harry Potter", "J.K. Rowling");
+        l2.mostrarDetalles();  
+        
+        Libro l3 = new Libro("El Señor de los Anillos", "J.R.R. Tolkien", 1200);
+        l3.mostrarDetalles();
+
+
+        CuentaBancaria cuenta = new CuentaBancaria("Maria", 500);
+        cuenta.depositar(200);
+        System.out.println(cuenta.getSaldo()); 
+        cuenta.retirar(1000);
+
+
+        Estudiante e = new Estudiante("Pedro", 21, 2.5);
+        e.mostrarInfo();
+    }
+}
